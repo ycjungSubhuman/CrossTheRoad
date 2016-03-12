3c7e9c032007ac7bc640d01a462260be08dea971
ycjungonvm <jung3519@gmail.com>
2016-03-12 22:35:27 +0900

    틀을 잡아봄

---
+++ Assn1/main.cpp
@@ -0,0 +1,52 @@
+#include<Windows.h>
+#include<GL/GL.h>
+#include<GL/glut.h>
+
+void init(void) {
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_FLAT);
+	/* Initiallize Objects */
+}
+void drawView(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	/* Implement view function*/
+
+	glutSwapBuffers();
+}
+void reshape(int w, int h) {
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, 1, 0, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+void processUserInput(int key, int x, int y) {
+
+	/* process User Input*/
+}
+
+void updateScene(int value)
+{
+	/* Implement Scene Update */
+
+	glutTimerFunc(20, updateScene, 0);
+	glutPostRedisplay();
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+	init();
+
+	glutDisplayFunc(drawView);
+	glutReshapeFunc(reshape);
+	glutSpecialFunc(processUserInput);
+	glutTimerFunc(20, updateScene, 0);
+	glutMainLoop();
+	return 0;
+}
