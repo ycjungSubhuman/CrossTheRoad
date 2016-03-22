9173550c5197673c20406e46f872966a141c5463
ycjungSubhuman <jung3519@gmail.com>
2016-03-22 23:55:00 +0900

    adding operator< on GScene

--- Assn1/GScene.h
+++ Assn1/GScene.h
@@ -1,14 +1,14 @@
 #pragma once
 #include "GObject.h"
-#include <list>
+#include <vector>
 
 class GScene {
 private:
-	std::list<GObject*> objects;
+	std::vector<GObject*> objects;
 public:
 	void drawAll();
 	GObject* addObject(GObject* obj);
 	GObject* removeObject(GObject* obj);
-	std::list<GObject*> getCollisions(GObject* obj); //get all colliding GObjects of obj
-	std::list<GObject*> getCollisionsOf(GObject* obj, std::string type); //get all colliding GObjects of obj with type "type"
+	std::vector<GObject*> getCollisions(GObject* obj); //get all colliding GObjects of obj
+	std::vector<GObject*> getCollisionsOf(GObject* obj, std::string type); //get all colliding GObjects of obj with type "type"
 };
