b8d1ce38e0e0234e4dd74bb43260ab61d167bbd9
ycjungonvm <jung3519@gmail.com>
2016-03-24 01:43:09 +0900

    minor typo

--- Assn1/Car.h
+++ Assn1/Car.h
@@ -6,7 +6,7 @@ class Car : public GObject {
 private:
 	double dis_y;
 public:
-	enum { CARWIDTH=10, CARHEIGHT=15, GENTIME=1500, SPEED=4 };//SPEED: per frame GENTIME:millisec
+	enum { CARWIDTH=10, CARHEIGHT=15, GENTIME=1000, SPEED=3 };//SPEED: per frame GENTIME:millisec
 	Car(int line, GameMap::Linetype linetype, int z=1);
 	virtual void draw();
 	virtual void frameAction();
