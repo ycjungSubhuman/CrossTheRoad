3cfcda371309ebbbbae89fe9ba19f1d2aa8801ef
ycjungSubhuman <jung3519@gmail.com>
2016-04-28 07:29:52 +0900

    Changed GModel::data_models to data_model

--- Assn1/GModel.cpp
+++ Assn1/GModel.cpp
@@ -2,32 +2,32 @@
 
 GModel::GModel(std::tuple<GLuint, double, double, double, int> data_models)
 {
-	this->data_models = data_models;
+	this->data_model = data_models;
 }
 
 GLuint GModel::getModelID()
 {
-	return std::get<0>(data_models);
+	return std::get<0>(data_model);
 }
 
 double GModel::getBoundboxX()
 {
-	return std::get<1>(data_models);
+	return std::get<1>(data_model);
 }
 
 double GModel::getBoundboxY()
 {
-	return std::get<2>(data_models);
+	return std::get<2>(data_model);
 }
 
 double GModel::getBoundboxZ()
 {
-	return std::get<3>(data_models);
+	return std::get<3>(data_model);
 }
 
 int GModel::getVertexSize()
 {
-	return std::get<4>(data_models);
+	return std::get<4>(data_model);
 }
 
 
