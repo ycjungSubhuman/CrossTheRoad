fcdac412003f3f65e2a092c0382eaac744d65228
ycjungSubhuman <jung3519@gmail.com>
2016-05-13 02:31:02 +0900

    GModel definition modified to have multiple textures

--- Assn1/GModel.cpp
+++ Assn1/GModel.cpp
@@ -11,9 +11,17 @@ GLuint GModel::getModelID()
 {
 	return std::get<0>(data_model);
 }
-GLuint GModel::getTextureID()
+GLuint GModel::getTextureID(TextureType type)
 {
-	return index_texturebuffer;
+	switch (type) {
+	case TEXTURE_DIFFUSE:
+		return index_texturebuffer;
+	case TEXTURE_NORMAL:
+		return index_normalmapbuffer;
+	default:
+		return -1; //non existing
+	}
+	
 }
 vec3 GModel::getPos() {
 	return std::get<1>(data_model);
@@ -39,16 +47,40 @@ int GModel::getVertexSize()
 	return std::get<3>(data_model);
 }
 
-void GModel::setTexture(std::string filename)
+void GModel::setTexture(TextureType type, std::string filename)
 {
-	glGenTextures(1, &index_texturebuffer);
-	glBindTexture(GL_TEXTURE_2D, index_texturebuffer);
-	texture = Texture(filename);
-	glTexImage2D(GL_TEXTURE_2D, 0, 
-		GL_RGB, texture.getWidth(), texture.getHeight(), 
-		0, GL_RGB, GL_FLOAT, texture.toArray());
+	Texture* current;
+	GLuint* pindex;
+	if (type == TEXTURE_DIFFUSE) {
+		current = &texture;
+		pindex = &index_texturebuffer;
+	}
+	else {
+		current = &normalmap;
+		pindex = &index_normalmapbuffer;
+	}
 
+	glGenTextures(1, pindex);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, *pindex);
+	*current = Texture(filename);
+	glTexImage2D(GL_TEXTURE_2D, 0, 
+		GL_RGB, current->getWidth(), current->getHeight(), 
+		0, GL_RGB, GL_FLOAT, current->toArray());
 }
+
+bool GModel::isTextureSet(TextureType type)
+{
+	switch (type) {
+	case TEXTURE_DIFFUSE:
+		return texture.isValid();
+	case TEXTURE_NORMAL:
+		return normalmap.isValid();
+	default:
+		return false;
+	}
+}
+
 
 GModel::~GModel()
 {
--- Assn1/GModel.h
+++ Assn1/GModel.h
@@ -12,17 +12,25 @@ class GModel {
 private:
 	std::tuple<GLuint, vec3, vec3, int> data_model;
 	GLuint index_texturebuffer;
+	GLuint index_normalmapbuffer;
 	Texture texture;
+	Texture normalmap;
 public:
+	//types of texture. diffuse for image mapping(ex: banana peel)
+	//normal for surface normal mapping
+	enum TextureType { TEXTURE_DIFFUSE, TEXTURE_NORMAL };
 	GModel();
 	GModel(std::tuple<GLuint, vec3, vec3, int> data_models);
 	GLuint getModelID();
-	GLuint getTextureID();
+	//returns the buffer id of given texture type
+	GLuint getTextureID(TextureType type); 
 	vec3 getPos();
 	double getBoundboxX();
 	double getBoundboxY();
 	double getBoundboxZ();
 	int getVertexSize();
-	void setTexture(std::string filename);
+	//set specified texture for this model with given file.
+	void setTexture(TextureType type, std::string filename);
+	bool isTextureSet(TextureType type); //returns if the specified texture is set
 	~GModel();
 };
