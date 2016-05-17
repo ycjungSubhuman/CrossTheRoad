2b801c6fc75894e426ee918bd1037930bfaf809f
Ria-Fox <jspak3@naver.com>
2016-05-17 21:49:01 +0900

    fragshader

--- Assn1/FragmentShader1.glsl
+++ Assn1/FragmentShader1.glsl
@@ -12,6 +12,7 @@ uniform sampler2D sample_texture; //unit 0
 uniform sampler2D sample_normalmap; //unit 1
 uniform bool isTextured; //if true, render with color_out
 uniform bool isNormaled;
+uniform int mode_shader;
 out vec3 color;
 
 void main()
@@ -26,8 +27,6 @@ void main()
 	vec3 E = normalize(fE);
 	vec3 L1 = normalize(fL1);
 	vec3 L2 = normalize(fL2);
-	vec3 H1 = normalize( L1 + E ); 
-	vec3 H2 = normalize( L2 + E ); 
 	vec4 product = vec4(0.4,0.4,0.4,1.0);
 
 	vec4 ambient = product;
@@ -37,10 +36,25 @@ void main()
 	float Kd2 = max(dot(L2, N), 0.0);
 	vec4 diffuse2 = Kd2*product;
 
-	float Ks1 = pow(max(dot(N, H1), 0.0), 0.3);
-	vec4 specular1 = Ks1*product;
-	float Ks2 = pow(max(dot(N, H2), 0.0), 0.3);
-	vec4 specular2 = Ks2*product;
+	vec4 specular1, specular2;
+	if(mode_shader == 2)
+	{
+		vec3 R1 = normalize( reflect(L1, N) );
+		vec3 R2 = normalize( reflect(L2, N) );
+		float Ks1 = pow(max(dot(R1, E), 0.0), 0.3);
+		specular1 = Ks1*product;
+		float Ks2 = pow(max(dot(R2, E), 0.0), 0.3);
+		specular2 = Ks2*product;
+	}
+	else if(mode_shader == 3)
+	{
+		vec3 H1 = normalize( L1 + E ); 
+		vec3 H2 = normalize( L2 + E ); 
+		float Ks1 = pow(max(dot(N, H1), 0.0), 0.3);
+		specular1 = Ks1*product;
+		float Ks2 = pow(max(dot(N, H2), 0.0), 0.3);
+		specular2 = Ks2*product;
+	}
 
 	vec4 lightcolor = ambient + diffuse1 + diffuse2 + specular1 + specular2;
 
